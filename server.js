34ff92b697a0bef8e963c0508cd1f61b3aba1981
tony <anthony.ervin10@gmail.com>
2024-07-15 15:54:22 -0700

    Added vercel scripts

--- server.js
+++ server.js
@@ -2,13 +2,16 @@
 import express from 'express'
 import bodyParser from "body-parser";
 import cors from 'cors';
+import morgan from 'morgan'; // Import Morgan
+
 import db from './db.js'
+
 import routes from './routes/routes.js'
 
 const app = express();
 
 app.use(bodyParser.json());
-
+app.use(morgan('dev')); // Use Morgan with 'dev' format
 app.use(cors());
 app.use('/', routes)
 
